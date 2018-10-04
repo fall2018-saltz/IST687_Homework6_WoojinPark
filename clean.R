c4ea8dbdb62f979b2608e6dd474f425854dfbc5b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:33:23 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,15 @@
 
 clean_data <- raw_data
+myfunction <-function()
+{
+  dfStates<-dfStates[-53,]
+  dfStates<-dfStates[-1,]
+  cnames <- colnames(dfStates)
+  cnames[5] <-"stateName"
+  cnames[6] <-"population"
+  cnames[7] <-"popOver18"
+  cnames[8] <-"percentOver18"
+  colnames(dfStates) <-cnames
+  dfStates<-dfStates[,-1:-4]
+  return(dfStates)
+}
